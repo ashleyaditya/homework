2c1d71ef596a43274c590ba9100f45bcefd73713
LAPTOP-O3MJRTK1\ashle <ashley.aditya@gmail.com>
2022-06-25 17:46:38 +1000

    second

--- hm1-transit-application/script.js
+++ hm1-transit-application/script.js
@@ -10,8 +10,23 @@ const stations = [
   "Newtown",
 ]
 
-const travelForm = (startStation, endStation) => {
+const travelForm = (stationS, stationE) => {
   let numberOfStops
+  const station1 = stationS.split(" ")
+  let startStation = station1
+    .map((word) => {
+      return word[0].toUpperCase() + word.substring(1).toLowerCase()
+    })
+    .join(" ")
+
+  const station2 = stationE.split(" ")
+  let endStation = station2
+    .map((word) => {
+      return word[0].toUpperCase() + word.substring(1).toLowerCase()
+    })
+    .join(" ")
+
+  console.log(startStation)
   if (
     stations.indexOf(startStation) != -1 &&
     stations.indexOf(endStation) != -1 &&
@@ -36,10 +51,12 @@ const travelForm = (startStation, endStation) => {
 }
 // Print the number of stops between the two stations. (e.g. log out "To go from 'Circular Quay' to 'Central' - you'll need to go through 2 stops")
 travelForm("Wynyard", "Museum")
-// Make this work if you want to go backwards! e.g. 'You want to travel from "Redfern" to "St. James"'
+// // // Make this work if you want to go backwards! e.g. 'You want to travel from "Redfern" to "St. James"'
 travelForm("Museum", "Wynyard")
-// If someone passes in a station that doesn't exist, tell them
+// // // If someone passes in a station that doesn't exist, tell them
 travelForm("Mugjh", "Wynyard")
+// Make it case-insensitive - there should be no difference between "redfern", "Redfern" and "REDFERN"
+travelForm("CirculaR QuAy", "Museum")
 // Printout the names of stations in seprate lines
 stations.map((station) => {
   console.log(station)
